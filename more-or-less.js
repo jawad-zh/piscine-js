aa564b3d93a55d6300131ba9da20d48aebe745c3
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-26 14:23:56 +0100

    more or less

---
+++ more-or-less.js
@@ -0,0 +1,12 @@
+function more(n){
+    return n+1
+}
+function less(n){
+    return n-1
+}
+function add(first , second){
+    return first + second
+}
+function sub(first ,second){
+    return first * second
+}
