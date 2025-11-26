eb022b14ef4c2fe07bb70b757dd6b96f568287be
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-26 14:17:00 +0100

    mulability

---
+++ mutability.js
@@ -0,0 +1,10 @@
+const person = {
+  name: 'Rick',
+  age: 77,
+  country: 'US',
+}
+let clone1 = {...person};
+let clone2 = {...person};
+let samePerson = person
+person.age+=1
+person.country= 'FR'
