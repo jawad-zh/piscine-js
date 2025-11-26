edec81221a2e13e9cdbde1f92efcabf6b36b8596
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-26 15:01:23 +0100

    last first

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,3 +1,4 @@
 const first = (arr)=> arr[0];
-const last =(arr) => arr[arr.length];
+const last =(arr) => arr[arr.length-1];
 const kiss = (arr) => [last(arr),first(arr)]
+console.log(kiss([1,2,3]))
