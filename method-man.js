fa942dbc778680accfefeb443da000af64acc395
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-26 17:18:23 +0100

    method-man

---
+++ method-man.js
@@ -0,0 +1,15 @@
+function words(s){
+    return s.split(" ")
+}
+function sentence(arr){
+    arr.join(" ")
+}
+function yell(s){
+    return s.toUpperCase()
+}
+function whisper(s){
+    return `*${s.toLowerCase()}*`
+}
+function capitalize(s){
+    return yell(s[0])+s.slice(1).toLowerCase()
+}
