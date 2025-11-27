7f0a0a276c217b70aede671506a5ba2bb1ea5d4e
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-27 13:02:40 +0100

    physics

--- physics.js
+++ physics.js
@@ -1,10 +1,10 @@
 function getAcceleration(test){
     if(test.f !== undefined && test.f!=0 && test.m !== undefined && test.m !=0 ){
         return test.f / test.m
-    }else if (test.Δv !== undefined && test.Δv !== 0 && test.Δt!= undefined && test.Δt!= 0 ){
-        return Δv / Δt
+    }else if (test.Δv !== undefined && test.Δv !== 0 && test.Δt!== undefined && test.Δt!== 0 ){
+        return test.Δv / test.Δt
     }else if (test.t !== undefined  && test.t !== 0 && test.d !== undefined && test.d !== 0 ){
-        return 2*test.d / t**2
+        return 2*test.d / test.t**2
     }else{
         return "impossible"
     }
