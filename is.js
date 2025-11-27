aca4590d025ccb9e1c00922d6d495df763ed4743
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-27 11:08:12 +0100

    is

--- is.js
+++ is.js
@@ -1,13 +1,12 @@
-const is = {
-    num : (arg)=> typeof(arg) === 'number' ,
-    nan :  (arg)=> Number.isNaN(arg) , //
-   str :  (arg)=> typeof(arg) === 'string', 
-   bool :  (arg)=> typeof(arg) === 'boolean', 
-   undef :  (arg)=> typeof(arg) === 'undefined',
-   def :  (arg)=> typeof(arg) !== 'undefined' ,
-   arr :  (arg)=> Array.isArray(arg)  ,
-    obj :  (arg)=> typeof(arg) === ('object')  ,
-    fun :  (arg)=> typeof(arg) === 'function' ,
-    truthy :  (arg)=> arg === true ,
-    falsy :  (arg)=> arg === false ,
-}
+
+    is.num : (arg)=> typeof(arg) === 'number' ,
+    is.nan :  (arg)=> Number.isNaN(arg) , //
+   is.str :  (arg)=> typeof(arg) === 'string', 
+   is.bool :  (arg)=> typeof(arg) === 'boolean', 
+   is.undef :  (arg)=> typeof(arg) === 'undefined',
+   is.def :  (arg)=> typeof(arg) !== 'undefined' ,
+   is.arr :  (arg)=> Array.isArray(arg)  ,
+    is.obj :  (arg)=> typeof(arg) === ('object')  ,
+    is.fun :  (arg)=> typeof(arg) === 'function' ,
+    is.truthy :  (arg)=> arg === true ,
+    is.falsy :  (arg)=> arg === false ,
