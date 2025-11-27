46c4ed3957b8421aa7c73164a28925d63e70fc88
jazahraoui <zahraoui.jawad8@gmail.com>
2025-11-27 11:11:13 +0100

    is

--- is.js
+++ is.js
@@ -6,7 +6,7 @@
    is.undef =  (arg)=> typeof(arg) === 'undefined'
    is.def =  (arg)=> typeof(arg) !== 'undefined' 
    is.arr =  (arg)=> Array.isArray(arg)  
-    is.obj =  (arg)=> typeof(arg) === ('object')  
+    is.obj =  (arg)=> (typeof(arg) === 'object' && !Array.isArray(arg))
     is.fun =  (arg)=> typeof(arg) === 'function' 
     is.truthy =  (arg)=> arg === true 
     is.falsy =  (arg)=> arg === false 
