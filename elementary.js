421010038d2da98b4a7e5c997556fac9f6287eb4
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-28 09:55:43 +0100

    elementary

--- elementary.js
+++ elementary.js
@@ -1,21 +1,67 @@
 function multiply(a,b){
     let result =0
+    let anum = 1
+    let bnumb =1
+    if(a < 0){
+        anum =0
+        a = -a
+    }
+    if (b < 0){
+        bnumb = 0
+        b = -b
+    }
     for (let i = 0 ; i < b ; i++){
         result += a
     }
-    return result
+    if ((anum === 0 && bnumb===0)||(anum===1 && bnumb===1)){
+        return result
+    }else {
+        return -result
+    }
+   
 }
 function divide(a,b){
     let count =0;
+    let anum = 1
+    let bnumb =1
+    if(a < 0){
+        anum =0
+        a = -a
+    }else if (a===0){
+        return 0
+    }
+    if (b < 0){
+        bnumb = 0
+        b = -b
+    }else if (b === 0){
+        return Infinity
+    }
+    
   while(a>0){
     a -= b
     count++
   }
-  return count
+  if ((anum === 0 && bnumb===0)||(anum===1 && bnumb===1)){
+        return count
+    }else {
+        return -count
+    }
 }
 function modulo(a,b){
-     while(a>0){
-    a -= b
-  }
-  return a
+    let numa = 1
+    if (a < 0){
+        a = -a
+        numa =0
+    }
+    if (b ===  0){
+        return NaN
+    }
+    while(a > 0){
+        a -= b
+    }
+    if (numa ===0){
+        return -a
+    }else{
+        return a
+    }
 }
