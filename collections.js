c83e934d74a6d87fa3b25e07952eff5d1f9f6083
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-29 08:34:14 +0100

    collections

--- collections.js
+++ collections.js
@@ -3,7 +3,7 @@ const arrToStr = (arr) => arr.join("")
 const setToArr = (set) => [...set]
 const setToStr = (set) => [...set].join("")
 const strToArr = (str) => str.split("")
-const strToSet = (str) => new Set(...str)
+const strToSet = (str) => new Set([...str])
 const mapToObj = (map) => Object.fromEntries(map)
 const objToArr = (obj) => Object.entries(obj);
 const objToMap = (obj) => Object.entries(obj);
@@ -13,5 +13,12 @@ const strToObj= (str) => {return {...str}};
 
 function superTyperOf(arg){
    let typeArr= Object.prototype.toString.call(arg).split(" ")
-   return typeArr[1].slice(0,typeArr[1].length-1)
+   let type = typeArr[1].slice(0,typeArr[1].length-1)
+   if (type ==='Null'){
+    return type[0].toLocaleLowerCase()+ type.slice(1)
+   }else if(type === 'Undefined'){
+    return type[0].toLocaleLowerCase()+ type.slice(1)
+   }else{
+    return type
+   }
 }
