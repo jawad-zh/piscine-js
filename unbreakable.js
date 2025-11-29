66721834c38db83cefc567c0bc5832cc923ad40f
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-29 10:30:31 +0100

    split

--- unbreakable.js
+++ unbreakable.js
@@ -1,29 +1,42 @@
-function split(string, sepilator){
+function split(string ,seperator){
+    // let first = true
+    // let tracker =0
+    let count =0;
+    let found = false;
+    let sLenght = seperator.length
+    let fixsLenght = seperator.length ;
     let word =""
     let words =[]
-    for (let i=0 ; i < string.length ; i++){
-        if (string[i] != sepilator){
-            word += string[i]
-    }else{
-        if (word != ""){
-            words.push(word)
-            word = ""
+    if (string.length < sLenght){
+        return string
+    }
+    for (let i =0 ; i < string.length ; i++){
+        if(found){
+            console.log("count",count,"fixlenght:",fixsLenght)
+            count ++
+            if (count ===fixsLenght){
+                found =false
+                count =0
+            }else{
+
+                continue
+            }
         }
+        let flag = string.substring(i,i+sLenght)
+        
+        if (flag !== seperator){
+           
+            word += string[i]
+            console.log(i,word)
+        }else{
+            found = true
+            words.push(word)
+            word =""
+        }
+        
     }
-}
-    if (word != ""){
         words.push(word)
-    }
+    
     return words
 }
-
-function join(arr,joiner){
-    let result = "";
-    for (let i =0 ; i < arr.length ; i++){
-        result += arr[i]
-        if (i!==arr.length-1){
-            result += joiner
-        }
-    }
-    return result
-}
+console.log(split("kjkrrkj","rr"))
