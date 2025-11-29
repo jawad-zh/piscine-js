50793afb010e3593622cc96d2edf86a3ceebbbeb
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-29 08:39:43 +0100

    collections

--- collections.js
+++ collections.js
@@ -5,9 +5,9 @@ const setToStr = (set) => [...set].join("")
 const strToArr = (str) => str.split("")
 const strToSet = (str) => new Set([...str])
 const mapToObj = (map) => Object.fromEntries(map)
-const objToArr = (obj) => Object.entries(obj);
-const objToMap = (obj) => Object.entries(obj);
-const arrToObj = (arr) => Object.fromEntries(arr);
+const objToArr = (obj) => Object.values(obj);
+const objToMap = (obj) => new map(Object.entries(obj));
+const arrToObj = (arr) => {return {...arr}};
 const strToObj= (str) => {return {...str}};
 
 
