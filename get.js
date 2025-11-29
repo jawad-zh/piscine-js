df97c01659ef3edaec354aa4321df85b4f5cfe56
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-29 12:55:26 +0100

    get

--- get.js
+++ get.js
@@ -1,12 +1,12 @@
 function get(src,path){
    let splited = path.split(".")
    let key = splited[0]
-   if (src[key]=== undefined){
+   if (src[key]== undefined){
     return undefined
    }
    let left = splited.slice(1)
    if (path.split(".").length > 1){
-    return get(src[key],left.join(""))
+    return get(src[key],left.join("."))
    }
    return src[key]
 }
