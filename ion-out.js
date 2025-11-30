1ea6f432b42c09b0b058b8782118a7fda5205b12
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-30 13:45:58 +0100

    ion out

--- ion-out.js
+++ ion-out.js
@@ -1,12 +1,10 @@
 function ionOut(str){
-    const regex = /\w+(ion)/g
+    const regex = /\w+t+(ion)/g
     let arr = str.match(regex)
     if (arr === null){
         return []
     }
     for(let i =0 ; i < arr.length ;i++){
-        arr[i]=arr[i].replace(/(ion)/,"")
-        arr[i]+= "t"
-    }
+        arr[i]=arr[i].replace(/(ion)/,"")    }
     return arr
 }
