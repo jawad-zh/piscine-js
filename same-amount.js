d8de08e24185b824105e13babcf83811563b486d
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-30 13:02:49 +0100

    same mount

--- same-amount.js
+++ same-amount.js
@@ -3,6 +3,9 @@ function sameAmount(str,regex1,regex2){
     regex2 = new RegExp(regex2,"g")
     let amount1= str.match(regex1)
     let amount2=str.match(regex2)
+    if (amount1 === null || amount2 === null){
+        return false
+    }
     if (amount1.length === amount2.length){
         return true
     }
