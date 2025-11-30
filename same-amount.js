0253205a191a8c57c335e1ef425f5054c5d19da2
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-30 12:58:57 +0100

    same amount

--- same-amount.js
+++ same-amount.js
@@ -1,4 +1,6 @@
 function sameAmount(str,regex1,regex2){
+    regex1 = new RegEx(regex1,"g")
+    regex2 = new RegEx(regex2,"g")
     let amount1= str.match(regex1)
     let amount2=str.match(regex2)
     if (amount1.length === amount2.length){
@@ -6,3 +8,4 @@ function sameAmount(str,regex1,regex2){
     }
     return false
 }
+
