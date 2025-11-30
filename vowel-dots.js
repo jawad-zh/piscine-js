d940f315a1f394e57f4452303d7dc5ed43e28e26
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-30 11:47:38 +0100

    vowel dots

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,12 +1,4 @@
+const vowels = /([aeiou])/gi
 function vowelDots(str){
-    const regex = /[aeiou]/g
-    let vowels =""
-    for (let i =0 ; i < str.length; i++){
-        if(str[i].match(regex)){
-            vowels +=`${str[i]}.`
-        }else{
-            vowels += str[i]
-        }
-    }
-    return vowels
+    return str.replace(vowels,"$1.")
 }
