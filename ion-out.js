831f5206285813c1c64b232689eb90843023c351
jawad-zh <zahraoui.jawad20@gmail.com>
2025-11-30 13:35:40 +0100

    ion out

---
+++ ion-out.js
@@ -0,0 +1,12 @@
+function ionOut(str){
+    const regex = /\w+(ion)+t/g
+    let arr = str.match(regex)
+    if (arr === null){
+        return []
+    }
+    for(let i =0 ; i < arr.length ;i++){
+        arr[i]=arr[i].replace(/(ion)/,"")
+    }
+    return arr
+}
+console.log(ionOut('functiont'))
