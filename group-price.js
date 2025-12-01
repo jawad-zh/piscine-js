1d53e4162331844adab52d89d32f52e90d1037d4
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-01 20:36:53 +0100

    group price

--- group-price.js
+++ group-price.js
@@ -1,18 +1,15 @@
 function groupPrice(str){
     let all = str.match(/[(USD)|($)]+\d+.\d+/g)
+    console.log("all",all)
     let normal = str.match(/(?<=[(USD)|($)])\d+(?=.)/g)
     let decimal = str.match(/(?<=\.)\d+/g)
     if (all === null){
         return []
     }
-    let temp = []
     let result =[]
     for (let i=0 ; i < all.length ; i++){
-        temp.push(`${all[i]}`)
-        temp.push(normal[i])
-        temp.push(decimal[i])
-        result.push(temp)
+         let temp =`${all[i]},${normal[i]},${decimal[i]}`
+        result.push(temp.split(','))
     }    
     return result
 }
-console.log(groupPrice('USD19.98', '19', '98'));
