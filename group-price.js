1ebfb5484707083530bcad24f65b2dd3d65575a6
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-01 20:06:05 +0100

    group price

---
+++ group-price.js
@@ -0,0 +1,14 @@
+function groupPrice(str){
+    let all = str.match(/(?<=(USD))\d+.\d+/g)
+    let normal = str.match(/(?<=(USD))\d+(?=.)/g)
+    let decimal = str.match(/(?<=\.)\d+/g)
+    let temp = []
+    let result =[]
+    for (i=0 ; i < all.length ; i++){
+        temp.push(`USD${all[i]}`)
+        temp.push(normal[i])
+        temp.push(decimal[i])
+        result.push(temp)
+    }    
+    return result
+}
