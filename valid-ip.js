59f4b3f01e3d1dd91220bec3e08d7cb5f564ed70
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-02 08:39:44 +0100

    valid ip

---
+++ valid-ip.js
@@ -0,0 +1,27 @@
+function findIP(str){
+    const regex = /\d+.\d+.\d+.\d+(:\d+)?/g
+    let arr = str.match(regex)
+    if (arr === null){
+        return []
+    }
+    let result = []
+
+        for (let i =0 ; i < arr.length ; i++){
+            let ip = (arr[i].match(/\d+.\d+.\d+.\d+/)).join("").split(".")
+            let port =(arr[i].match(/(?<=:)\d+/)).join("")
+            for (let j = 0 ; j < ip.length ;j++){
+                if (+ip[j]>=0 && +ip[j]<=255){
+                    if (j === +ip.length-1 && (+port <=65535 &&  +port>=0)){
+                        result.push(arr[i])
+                    }
+                }else{
+                    break
+                }
+            }
+            
+            
+            
+        }
+    
+    return result
+}
