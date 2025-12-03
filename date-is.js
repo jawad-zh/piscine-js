413bfff4a525e3bad7a096cb2787c215a79596de
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-03 18:07:35 +0100

    date

---
+++ date-is.js
@@ -0,0 +1,31 @@
+function isValid(date){
+    if (typeof(date) === 'string'){
+        return false
+    }
+    let flag = new Date(date).toString()
+    if (flag === "Invalid Date"){
+        return false
+    }
+    return true
+}
+function isAfter(date1,date2){
+    return date1 > date2
+}
+function isBefore(date1,date2){
+    return date2 > date1
+}0
+function isFuture(date){
+    let now = Date.now()
+  if (isValid(date) && now < date){
+    return true
+  }  
+  return false
+}
+function isPast(isPast){
+      let now = Date.now()
+  if (isValid(date) && now > date){
+    return true
+  }  
+  return false
+}
+
