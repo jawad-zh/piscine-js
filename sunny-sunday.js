d7aa4b311dcee310aaaf83e7cb5b745373f907a0
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-04 20:35:29 +0100

    sunny sunday

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -6,7 +6,7 @@ const days = [
     "Friday",
     "Saturday"
 ]
-function addWeek(date){
+function sunnySunday(date){
     let start = new Date("0001-01-01")
    let diff = (date - start) /1000/60/60/24
     return days[diff%6]
