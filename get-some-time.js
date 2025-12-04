29ea0abba1fff5f4983476febe4ac904c55a2504
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-04 10:07:56 +0100

    get some time

--- get-some-time.js
+++ get-some-time.js
@@ -1,19 +1,13 @@
-// 
-    function firstDayWeek(week, year) {
+function firstDayWeek(week, year) {
     let format = '01-01-' + year 
     let date = new Date(format)
-    let day = date.getDay()
-    console.log(day);
-    
+    let day = date.getDay()    
     if (day === 0){
         day += 7
     }
     date.setDate(((date.getDate()-(day -1) + ((week-1)*7)) ))
-    if (date.getFullYear < year){
-        console.log("date",date.getFullYear(),year);
-        
+    if (date.getFullYear() < year){        
         return '01-01-' + year
     }
     return  `${String(date.getDay()).padStart(2,'0')}-${String(date.getMonth()+1).padStart(2,'0')}-${String(date.getFullYear()).padStart(4,'0')}`
 }
-
