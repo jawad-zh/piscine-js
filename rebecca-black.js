4ce18b896c3665be7ae3545e4415ac83c1e54531
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-04 14:53:42 +0100

    rebecca

---
+++ rebecca-black.js
@@ -0,0 +1,16 @@
+function isFriday(date){
+    let day = date.getDay()
+    return day === 5
+}
+function isWeekend(date){
+    let day = date.getDay()
+    return day ==0 || day == 6
+}
+function isLeapYear(date){
+    let year = date.getFullYear()
+    return (year%4 ==0 && year %100 != 0 || year%400==0)
+}
+function isLastDayOfMonth(date){
+    let NewDate = new Date(date.getFullYear(),date.getMonth(),date.getDay()+1)
+    return date.getMonth() === NewDate.getMonth()
+}
