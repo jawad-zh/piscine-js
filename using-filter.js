7a9d976cbce9d3188c34ef60506bcaf07df37988
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 17:22:15 +0100

    using filter

--- using-filter.js
+++ using-filter.js
@@ -17,30 +17,11 @@ function filter1DistinctVowel(arr){
     console.log("----------",);
     
     return arr.filter(function(str){
-        let vawol = str.match(/[aeiuo]/g)
+        let vawol = str.toLowerCase().match(/[aeiuo]/g)
         let unique = new Set(vawol)
         let compar = [...unique]
-        // let compar = []  
-        // for (let i=0 ; i < str.length ; i++){
-        //     let push = true
-        //     if (str[i] == 'a'|| str[i] == 'u' || str[i] == 'o' || str[i] == 'e' || str[i] == 'i'){
-        //         for(let j =0 ; j < compar.length ; j++){
-        //         if (str[i] == compar[j]){
-        //             push = false
-        //         }
-        //     }
-        //     if (push){
-        //         compar.push(str[i])
-        //     }
-        //     }
-            
-        // }      
         if (compar.length === 1){            
-            if(str[str.length-1] === 'Utah'){
-                return str.slice(0,str.length-1)
-            }else{
                 return str
-            }
         }
     })
 }
@@ -55,13 +36,3 @@ function multiFilter(arr){
         }
     })
 }
-console.log(filter1DistinctVowel([
-    'Alabama',
-    'Alaska',
-    'Arkansas',
-    'Kansas',
-    'Maryland',
-    'Mississippi',
-    'New Jersey',
-    'Tennessee',
-  ]));
