2cd349679caa8e8916f0648b1b75b8d366eb2cb8
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 20:12:51 +0100

    filter

--- filter.js
+++ filter.js
@@ -7,3 +7,25 @@ function filter(arr,func){
     }
     return resutlt
 }
+function reject(arr,func){
+     let resutlt =[]
+    for (let i = 0 ; i < arr.length ; i++){
+        if (!func(arr[i],i,arr)){
+            resutlt.push(arr[i])
+        }
+    }
+    return resutlt
+}
+function partition(arr,func){
+    let result =[]
+    let ok=[]
+    let no = []
+    for (let i = 0 ; i < arr.length ; i++){
+        if(func(arr[i],i,arr)){
+            ok.push(arr[i])
+        }else{
+            no.push(arr[i])
+        }
+    }
+   return  result.push(ok,no)
+}
