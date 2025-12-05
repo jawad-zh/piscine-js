2bc658bf93f76ea056f0460741e378a69c899807
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 19:32:41 +0100

    mapper

--- mapper.js
+++ mapper.js
@@ -8,9 +8,9 @@ function map(arr,func){
 function flatMap(arr,func){
     let result =[]
     for (let i =0 ; i < arr.length ; i++){
-        let temp = func(arr[i],i,arr)
+        let temp = func(arr[i],i,arr)        
         if (Array.isArray(temp)){
-            result.push([...temp])
+            result.push(...temp)            
         }else{
             result.push(temp)
         }
