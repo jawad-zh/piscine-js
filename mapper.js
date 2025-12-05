d60a23d98990696b9f1a6aea41cca4616c215643
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 19:28:44 +0100

    mapper

--- mapper.js
+++ mapper.js
@@ -5,3 +5,15 @@ function map(arr,func){
     }
     return result
 }
+function flatMap(arr,func){
+    let result =[]
+    for (let i =0 ; i < arr.length ; i++){
+        let temp = func(arr[i],i,arr)
+        if (Array.isArray(temp)){
+            result.push([...temp])
+        }else{
+            result.push(temp)
+        }
+    }
+    return result
+}
