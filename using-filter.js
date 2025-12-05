f4225dde6bab14ab64ff96d0cdf59dbfd387c734
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 16:54:34 +0100

    using filter

---
+++ using-filter.js
@@ -0,0 +1,36 @@
+function filterShortStateName(arr){
+    return arr.filter(function(str){
+        return str.length < 7
+    })
+}
+function filterStartVowel(arr){
+    return arr.filter(function(str){
+        return (str.match(/^(a|e|i|o|u)/))
+    })
+}
+function filter5Vowels(arr){
+    return arr.filter(function(str){
+         return (5 <=str.match(/[aueio]/g).length)
+    })
+}
+function filter1DistinctVowel(arr){
+    return arr.filter(function(str){
+        let vawol = str.match(/[aeiuo]/g)
+        let unique = new Set(vawol)
+        let compar = [...unique]        
+        if (compar.length === 1){
+            return str
+        }
+    })
+}
+function multiFilter(arr){
+ return  arr.filter(function(obj){
+        if (obj.capital.length>=8 && 
+            /^[^aeiuo]/.test(obj.name)&&
+            /[aeiuo]/.test(obj.tag)&&
+            obj.region !== 'South'
+        ){
+            return obj
+        }
+    })
+}
