8991f2dc04c5215c477378c3606062c4df32a28d
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 18:39:55 +0100

    using reduce

--- using-reduce.js
+++ using-reduce.js
@@ -1,19 +1,18 @@
-const adder = (arr,num = 0) => arr.reduce((acc , current) => acc + current,num)
-function sumOrMul(arr,num){
-    return  arr.reduce(function(acc,current){
-        if (current%2==0){
+const adder = (arr, num = 0) => arr.reduce((acc, current) => acc + current, num)
+function sumOrMul(arr, num = 0) {
+    return arr.reduce(function (acc, current) {
+        if (current % 2 == 0) {
             acc *= current
-        }else{
-            acc+= current
+        } else {
+            acc += current
         }
         return acc
-    },num)
-
+    }, num)
 }
 
-function funcExec(arr,num = 0){
-  return   arr.reduce(function (acc,fun){
+function funcExec(arr, num = 0) {
+    return arr.reduce(function (acc, fun) {
         return fun(acc)
-    },num)
-    
+    }, num)
+
 }
