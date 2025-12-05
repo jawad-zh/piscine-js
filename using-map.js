8360a3565ab44d759482f2ee113c75f062db3e10
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 15:49:45 +0100

    using map

--- using-map.js
+++ using-map.js
@@ -13,8 +13,10 @@ function upperCasingStates (arr){
 
 function fahrenheitToCelsius(arr) {
     return arr.map(function(ele){
-        if (/\d+(?=°F)/g.test(ele)){
-            ele = `${(Math.floor(((ele.match(/\d+(?=°F)/g)-32)*(5/9))))}°C`
+        console.log("-----------",ele.match(/(-)?\d+(?=°C)/g));
+        if (/(-)?\d+(?=°F)/g.test(ele)){
+
+            ele = `${(Math.floor(((ele.match(/(-)?\d+(?=°F)/g)-32)*(5/9))))}°C`
         }
          return ele ;
     },)
@@ -28,7 +30,7 @@ function trimTemp(arr){
 }
 function tempForecasts(arr){
    let result = arr.map(function(obj){
-        const regex = /\d+\s?(?=°F)/g        
+        const regex = /(-)?\d+\s?(?=°F)/g        
         obj.temperature = Math.round((obj.temperature.match(regex)-32)*(5/9))
         obj.state = obj.state[0].toUpperCase()+obj.state.slice(1)
         return `${obj.temperature}°Celsius in ${obj.city}, ${obj.state}`
