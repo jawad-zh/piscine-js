e3fe9bc2ffb67043bd80e64ceac1285ef5cd1530
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-05 20:40:48 +0100

    reduce

--- reduce.js
+++ reduce.js
@@ -5,3 +5,18 @@ function fold(arr,func,acc=0){
     return acc
 }
 
+function foldRight(arr,func,acc=0){
+    for (let i = arr.length-1 ; i >=0 ; i--){
+        acc = func(acc,arr[i])
+    }
+    return acc
+}
+function reduce(arr,func,acc=0){
+    if (arr.length>=1){
+        acc = acc ? acc : arr[0]
+    }
+    for (let i = acc==0?1:acc ; i<arr.length ; i++){
+        acc = func(acc,arr[i])
+    }
+    return acc
+}
