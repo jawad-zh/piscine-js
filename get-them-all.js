c538ce56780b120eb88b34b0cd1a1a88143c602e
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-06 14:38:50 +0100

    get-them-all

--- get-them-all.js
+++ get-them-all.js
@@ -1,6 +1,12 @@
 function getArchitects(){
-    let architects = document.getElementsByTagName("a")
-    let all = document.querySelectorAll("*")
+    let architects = Array.from(document.getElementsByTagName("a"))
+    let all = Array.from(document.querySelectorAll("*"))
     let NonArchitects = all.filter(ele=> !architects.includes(ele))
     return [architects,NonArchitects]
+}
+function getClassical(){
+    let [architects,_] = getArchitects()
+    let classicale = architects.filter(ele => ele.classList.contains("classical"))
+    let noCalassical = architects.filter(ele => !classicale.includes(ele))
+    return [classicale,noCalassical]
 }
