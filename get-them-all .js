034ebe53e043d7c5a3615bf5dc69980447d0c7d9
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-06 18:07:19 +0100

    get them all

--- get-them-all .js
+++ get-them-all .js
@@ -7,19 +7,26 @@ export function getArchitects(){
     return result
 }
 export function getClassical(){
+    let result =[]
     let classical = Array.from(document.querySelectorAll('a.classical'))
-    let noclassical = Array.from(document.querySelectorAll('a:not(a.classical)'))
-    return [classical,noclassical]
+    let noclassical = Array.from(document.querySelectorAll('a:not(.classical)'))
+    result.push(classical)
+    result.push(noclassical)
+    return result
 }
 export function getActive(){
+    let result = []
     let classicalActive = Array.from(document.querySelectorAll('a.classical.active'))
     let classicalNotActiv = Array.from(document.querySelectorAll('a:not(.classical.active)'))
-    return [classicalActive,classicalNotActiv]
+    result.push(classicalActive)
+    result.push(classicalNotActiv)
+    return result
 }
 export function getBonannoPisano(){
     let result = []
     let bonannoPisano = document.getElementById('BonannoPisano')
-    let rest = Array.from(document.querySelectorAll('a.classical.active'))
-  result.push(bonannoPisano,rest)
+    let rest = Array.from(document.querySelectorAll('a.classical.active')).filter(a=> a.id != 'BonannoPusano')
+  result.push(bonannoPisano)
+  result.push(rest)
   return result
 }
