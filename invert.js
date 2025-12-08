1f0436f3cd99e1b2a2b7aec51395328622c7db82
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-08 14:25:05 +0100

    invert

---
+++ invert.js
@@ -0,0 +1,12 @@
+function invert(obj){
+    let result = {}
+    let keys = Object.keys(obj)
+    let values = Object.values(obj)
+    for (let i =0 ; i < keys.length ; i++){
+        result[values[i]]=keys[i]
+    }
+    return result
+}
+console.log(invert({
+    'jawad':'zahraoui'
+}));
