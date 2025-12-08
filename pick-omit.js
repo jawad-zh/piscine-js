64666123cf63587ff2d6326a1d3f259b4639475e
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-08 15:22:46 +0100

    pick omit

--- pick-omit.js
+++ pick-omit.js
@@ -16,7 +16,21 @@ function pick(obj,arrOrStr){
     }
     return result
 }
-console.log(pick({
-    'jawad':'ana',
-    'zahraoui':'howa'
-},['jawad','zahraoui']));
+function omit(obj,arrOrStr){
+    let result = {}
+    let keys = Object.keys(obj)
+    for (let i =0 ; i < keys.length ; i++ ){
+        if(Array.isArray(arrOrStr)){
+            for (let j =0 ; j < arrOrStr.length ; j++){
+                if (arrOrStr[j] != keys[i]){
+                    result[keys[i]]=obj[keys[i]]
+                }
+            }
+     }else{        
+        if(arrOrStr != keys[i]){            
+            result[keys[i]]= obj[keys[i]]
+        }
+     }
+    }
+    return result
+}
