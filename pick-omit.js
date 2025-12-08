8616f1a50ba6a33dcf15dac36c38b07bb92c2930
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-08 15:34:51 +0100

    pick omit

--- pick-omit.js
+++ pick-omit.js
@@ -17,14 +17,18 @@ function pick(obj,arrOrStr){
     return result
 }
 function omit(obj,arrOrStr){
+    let found = false
     let result = {}
     let keys = Object.keys(obj)
     for (let i =0 ; i < keys.length ; i++ ){
         if(Array.isArray(arrOrStr)){
             for (let j =0 ; j < arrOrStr.length ; j++){
-                if (arrOrStr[j] != keys[i]){
-                    result[keys[i]]=obj[keys[i]]
+                if (arrOrStr[j] === keys[i]){
+                    found = true
                 }
+            }
+            if (!found){
+                result[keys[i]] = obj[keys[i]]
             }
      }else{        
         if(arrOrStr != keys[i]){            
@@ -34,3 +38,11 @@ function omit(obj,arrOrStr){
     }
     return result
 }
+console.log(omit({
+    
+    'drill': 'bosh',
+  'grinders': 'DeWalt',
+   'saws': ' Makita'
+  
+
+},'grinders'));
