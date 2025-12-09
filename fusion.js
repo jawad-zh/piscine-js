3e03f22ecf43fa0f313e138682b8bf2f6ae2467c
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-09 12:56:27 +0100

    fixing key

--- fusion.js
+++ fusion.js
@@ -1,22 +1,21 @@
 function fusion(obj1, obj2) {
     let keysobj2 = Object.keys(obj2)
     for (let i =0 ; i < keysobj2.length ; i++ ){
-        if (obj1[keysobj2] === undefined){
-            boj1[keysobj2] = obj2[keysobj2]
+        if (obj1[keysobj2[i]] === undefined){
+            obj1[keysobj2[i]] = obj2[keysobj2[i]]
         }else{
-            if(Array.isArray(obj1[keysobj2]) && Array.isArray(obj2[keysobj2])){
-                obj1[keysobj2] = [...obj1[keysobj2],...obj2[keysobj2]]
-            }else if (typeof(obj1[keysobj2] == 'number') && typeof(obj2[keysobj2] == 'number')){
-                obj1[keysobj2] = obj1[keysobj2] + obj2[keysobj2]
-            }else if(typeof(obj1[keysobj2] == 'string') && typeof(obj2[keysobj2] == 'string')){
-                obj1[keysobj2] = `${obj1[keysobj2]} ${obj2[keysobj2]}`
-            }else if ( typeof(obj1[keysobj2]) == 'object' && typeof(obj2[keysobj2]) == 'object' ){
-                fusion(obj1[keysobj2],obj2[keysobj2])
+            if(Array.isArray(obj1[keysobj2[i]]) && Array.isArray(obj2[keysobj2[i]])){                
+                obj1[keysobj2[i]] = [...obj1[keysobj2[i]],...obj2[keysobj2[i]]]
+            }else if (typeof(obj1[keysobj2[i]] == 'number') && typeof(obj2[keysobj2[i]] == 'number')){
+                obj1[keysobj2[i]] = obj1[keysobj2[i]] + obj2[keysobj2[i]]
+            }else if(typeof(obj1[keysobj2[i]] === 'string') && typeof(obj2[keysobj2[i]] === 'string')){
+                obj1[keysobj2[i]] = `${obj1[keysobj2[i]]} ${obj2[keysobj2[i]]}`
+            }else if ( typeof(obj1[keysobj2[i]]) == 'object' && typeof(obj2[keysobj2[i]]) == 'object' ){
+                fusion(obj1[keysobj2[i]],obj2[keysobj2])
             }else{
-                obj1[keysobj2] = obj2[keysobj2]
+                obj1[keysobj2[i]] = obj2[keysobj2[i]]
             }
         }
     }
     return obj1
 }
-console.log(fusion({ nbr: 12 }, { nbr: 23 }));
