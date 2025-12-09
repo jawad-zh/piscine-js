dbdb62d6b05c3d07425207f74f6a8d3caa413b72
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-09 19:06:07 +0100

    manipulate key

---
+++ manipulate-keys.js
@@ -0,0 +1,27 @@
+function filterKeys(obj,func){
+    let result = {}
+    for (let key in obj){
+        if (func(key)){
+            result[key] = obj[key]
+        }
+    }
+    return result
+}
+function mapKeys(obj,func){
+    let result = {}
+    for (let key in obj){
+        let newKey = func(key)
+        result[newKey]=obj[key]
+    }
+    return result
+}
+
+function reduceKeys(obj,func,acc){
+    let keys = Object.keys(obj)
+    acc = acc ? acc : keys[0]
+    keys = keys.slice(1)
+    for (let i =0 ; i < keys.length ; i++){
+        acc = func(acc,keys[i])
+    }
+    return acc
+}
