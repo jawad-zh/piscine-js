79e681c5f97696720a5212e9446bffaf445e406d
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-09 12:45:02 +0100

    fusion

--- fusion.js
+++ fusion.js
@@ -6,10 +6,10 @@ function fusion(obj1, obj2) {
         }else{
             if(Array.isArray(obj1[keysobj2]) && Array.isArray(obj2[keysobj2])){
                 obj1[keysobj2] = [...obj1[keysobj2],...obj2[keysobj2]]
-            }else if(typeof(obj1[keysobj2] == 'string') && typeof(obj2[keysobj2] == 'string')){
-                obj1[keysobj2] = `${obj1[keysobj2]} ${obj2[keysobj2]}`
             }else if (typeof(obj1[keysobj2] == 'number') && typeof(obj2[keysobj2] == 'number')){
                 obj1[keysobj2] = obj1[keysobj2] + obj2[keysobj2]
+            }else if(typeof(obj1[keysobj2] == 'string') && typeof(obj2[keysobj2] == 'string')){
+                obj1[keysobj2] = `${obj1[keysobj2]} ${obj2[keysobj2]}`
             }else if ( typeof(obj1[keysobj2]) == 'object' && typeof(obj2[keysobj2]) == 'object' ){
                 fusion(obj1[keysobj2],obj2[keysobj2])
             }else{
@@ -19,3 +19,4 @@ function fusion(obj1, obj2) {
     }
     return obj1
 }
+console.log(fusion({ nbr: 12 }, { nbr: 23 }));
