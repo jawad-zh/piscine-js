b19134c070771b427b86dc2e7ec7eeaf9681e61c
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-10 23:48:22 +0100

    replica

--- replica.js
+++ replica.js
@@ -1,7 +1,7 @@
 function replica  (targetObj, ...object)  {
     for (const obj of object) {
         for (const key in obj) {
-            if (!Array.isArray(targetObj[key]) && typeof targetObj[key] === "object" ) {
+            if (!Array.isArray(targetObj[key]) && typeof targetObj[key] === "object" && !Array.isArray(obj[key]) ) {
                 replica(targetObj[key], obj[key])
             } else {
                 targetObj[key] = obj[key]
