4cc0ad9276dff6ad97b852425bb19b39a07ded75
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-10 23:21:00 +0100

    deep copy

---
+++ deep-copy.js
@@ -0,0 +1,25 @@
+function deepCopy(objOrArr){
+    if (Array.isArray(objOrArr)){        
+        let arrayResult = []
+        for(let i =0 ; i < objOrArr ; i++){
+            if (typeof objOrArr[i] === 'object'){                
+                deepCopy(objOrArr[i])
+            }else{                
+                arrayResult.push(objOrArr[i])
+            }
+        }
+        return arrayResult
+    }else{
+            let object = {}
+            let keys = Object.keys(objOrArr)
+            for (let i =0 ; i < keys.length ; i++){
+                if (typeof objOrArr[keys[i]] === 'object'){
+                    deepCopy(objOrArr[keys[i]])
+                }else{
+                    object[keys[i]]= objOrArr[keys[i]]
+                }
+            }
+            return object
+        }
+}
+console.log(deepCopy([1, 'a']));
