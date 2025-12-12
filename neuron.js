77448c92f4c4ed2210993cb4906fc6ac42b3a15a
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-12 23:26:45 +0100

    neuron

---
+++ neuron.js
@@ -0,0 +1,38 @@
+function neuron(arr){
+    let result = {}
+    let order = false 
+    for (let i =0 ; i < arr.length ; i++){
+        let splited = arr[i].split('- Response:');
+        let response = splited[1].trim()
+        let quetion = splited[0].split(':')[1]  
+        let key = quetion.trim().split(' ').join('_').trim()
+        if (splited[0].split(':')[0] == 'Orders'){
+            order = true
+        }
+        if (!order){
+            if (!result['quetions']) {
+            result['quetions']={}
+        }
+        if (!result['quetions'][key]){
+        result['quetions'][key] ={
+            'question':key,
+                'responses': []
+        }
+        }
+        result.quetions[key].responses.push(response)
+        }else{
+              if (!result['orders']) {
+            result['orders']={}
+        }
+        if (!result['orders'][key]){
+        result['orders'][key] ={
+            'order':key,
+                'responses': []
+        }
+        }
+        result.orders[key].responses.push(response)
+        }
+        
+    }
+    return result
+}
