957e82c052f643ca0ba44bf217db6edcece16cee
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-12 19:43:13 +0100

    debounce

--- debounce.js
+++ debounce.js
@@ -1,10 +1,25 @@
     function debounce(func,await){
+        let id;
         return function(...arg){
-            let id;
             if( id ){
                 clearInterval(id)
             }
-             id =setTimeout(func(arg),await)
+             id =setTimeout(() => func(...arg),await)
         }
         
+    }
+
+    function opDebounce(func,await,leading=false){
+        let id;
+        return function(...arg){
+            if (leading){
+            func(...arg)
+            leading = false
+        }
+        if (id){
+            clearTimeout(id)
+        }
+        id = setTimeout(() => func(...arg) ,await)
+
+        }
     }
