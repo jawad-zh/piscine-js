7195d78544041a2e18ab71befb1a8295c2ea6222
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-13 17:24:37 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -9,24 +9,14 @@ function throttle(func,wait){
         ,wait) 
     }
 }
-function opThrottle(func,wait,leading){
-      let run = true 
-      let first = true
-      if (leading){
-        first = true
-      }else{
-        first = false
-      }
+function opThrottle(func,wait,leading=true,trailing){
+   let run = leading? true:false
     return function(...arg){
-        if (first){
-            func(...arg)
-            first = false
-        }
         if(!run) return
         func(...arg)
         run = false
         setTimeout(()=>
         run = true
         ,wait) 
-    }
+    } 
 }
