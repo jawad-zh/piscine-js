bb85a6b0de644057572aa8774d65144aa209415e
jawad-zh <zahraoui.jawad20@gmail.com>
2025-12-14 11:32:41 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -3,7 +3,7 @@ function interpolation({step,start,end,callback,duration}){
     let timeForStep = duration/step
     for (let i =1 ; i<= step ; i++){
         setTimeout(()=>{
-            let x = distanceStep * i
+            let x = start + distanceStep * i
             let y = timeForStep*i
             callback([x,y])
         },timeForStep*i)
